4d8446559a3e4fbe04c414c80244a65fb9a803fe
noeppi_noeppi <noeppinoeppi@gmail.com>
2023-05-19 13:46:35 +0200

    Version type javadoc

--- src/main/java/org/moddingx/launcherlib/launcher/VersionType.java
+++ src/main/java/org/moddingx/launcherlib/launcher/VersionType.java
@@ -2,9 +2,34 @@ package org.moddingx.launcherlib.launcher;
 
 import java.util.Locale;
 
+/**
+ * A minecraft version type.
+ */
 public enum VersionType {
-    ALPHA, BETA, SNAPSHOT, RELEASE;
+
+    /**
+     * The {@code old_alpha} version type.
+     */
+    ALPHA,
     
+    /**
+     * The {@code old_beta} version type.
+     */
+    BETA,
+    
+    /**
+     * The {@code snapshot} version type.
+     */
+    SNAPSHOT,
+    
+    /**
+     * The {@code release} version type.
+     */
+    RELEASE;
+
+    /**
+     * Gets a version type by id.
+     */
     public static VersionType get(String id) {
         return switch (id.toLowerCase(Locale.ROOT)) {
             case "alpha", "old_alpha" -> ALPHA;
